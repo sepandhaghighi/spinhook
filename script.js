f042b62ed113fda65e6d22b7ef75182311b77d17
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-08-21 20:24:04 +0330

    fix : add GameScene.playSound calls

--- script.js
+++ script.js
@@ -357,7 +357,7 @@ class GameScene extends Phaser.Scene {
     }
 
     attachToOrbit() {
-        this.sound.play('attach', {volume: 0.4});
+        this.playSound('attach', { volume: 0.4 });
         this.player.setVelocity(0, 0);
         this.trailEmitter.stop();
         this.burstEmitter.explode(35, this.player.x, this.player.y);
@@ -397,7 +397,7 @@ class GameScene extends Phaser.Scene {
     }
 
     gameOver() {
-        this.sound.play('death', {volume: 0.5});
+        this.playSound('death', { volume: 0.5 });
         Leaderboard.finishSession(currentUsername, this.score, playStartTime);
         this.playerState = PlayerState.DEAD;
         this.player.setVelocity(0, 0);
