717f9792b0dd0f4093f75daaf527f3e991050123
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-08-25 03:40:12 +0330

    rel : migrate to version 0.3

--- assets/version_check.py
+++ assets/version_check.py
@@ -5,7 +5,7 @@ import sys
 import codecs
 
 Failed = 0
-VERSION = "0.2"
+VERSION = "0.3"
 
 INDEX_ITEMS = ['<div class="version">Version {0}</div>']
 CHANGELOG_ITEMS = [
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SPINHOOK_VERSION = "v0.2";
+const SPINHOOK_VERSION = "v0.3";
 let currentScore = 0;
 let currentUsername = "";
 let playStartTime = null;
